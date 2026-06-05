520ff23ad8b277b7b873a1bdd119847d55582c9f
NCBarrett <80253863+NCBarrett@users.noreply.github.com>
2026-06-05 15:32:15 -0700

    Setup basics, to create skeleton

--- src/main/java/com/example/simplecalculator/Main.java
+++ src/main/java/com/example/simplecalculator/Main.java
@@ -2,19 +2,53 @@ package com.example.simplecalculator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        //        primaryStage.setScene(new Scene(new CustomerController().getView()));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+
+        // Create UI elements
+        VBox root = new VBox();
+        VBox outputBox = new VBox();
+        outputBox.setSpacing(10);
+        outputBox.setPadding(new Insets(10));
+
+        Label outputField = new Label("");
+        outputBox.setAlignment(Pos.BASELINE_RIGHT);
+        Label operationsField = new Label("Operations label");
+        Button button = new Button("1");
+
+        button.setOnAction(event -> {
+            outputField.setText(outputField.getText() + "1");
+        });
+
+        GridPane buttonGrid = new GridPane();
+
+        outputBox.getChildren().addAll(operationsField, outputField);
+        buttonGrid.add(button, 0, 0);
+
+        root.getChildren().addAll(outputBox, buttonGrid);
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+    //    public boolean operationComplete(String input) {
+//
+//        return input.matches(".*\\d$");
+//    }
 }
