8d5d2c158977be095f6d8b01bb42f8e7913713b2
NCBarrett <80253863+NCBarrett@users.noreply.github.com>
2026-06-09 11:51:21 -0700

    Rebuilding and fixing decimal and comma issues

--- src/main/java/com/example/simplecalculator/Main.java
+++ src/main/java/com/example/simplecalculator/Main.java
@@ -14,6 +14,9 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class Main extends Application {
 
@@ -30,70 +33,53 @@ public class Main extends Application {
         outputBox.setAlignment(Pos.BASELINE_RIGHT);
         Label operationsField = new Label("Operations label");
 
-        Button clearBtn = new Button("C");
-
-        clearBtn.setOnAction(event -> {
-            outputField.setText("");
-        });
-
         GridPane buttonGrid = new GridPane();
         buttonGrid.setPadding(new Insets(10, 10, 10, 10));
         buttonGrid.setHgap(10);
         buttonGrid.setVgap(10);
 
         outputBox.getChildren().addAll(operationsField, outputField);
-        buttonGrid.add(clearBtn, 0, 0);
 
-        //        Button button = new Button("1");
-//        button.setOnAction(event -> {
-//            outputField.setText(outputField.getText() + "1");
-//        });
-//        buttonGrid.add(button, 0, 1);
-
-        int startInt = 7;
-        for (int i = 0; i < 3; i++) {
-            String btnText = String.valueOf(i + startInt);
-        // System.out.println("button text = " + btnText);
-            Button numberButton = new Button(btnText);
-            numberButton.setOnAction(event -> {
-                outputField.setText(outputField.getText() + btnText);
-            });
-            buttonGrid.add(numberButton, i, 1);
-        }
-
-        startInt = 4;
-        for (int i = 0; i < 3; i++) {
-            String btnText = String.valueOf(i + startInt);
-            // System.out.println("button text = " + btnText);
-            Button numberButton = new Button(btnText);
-            numberButton.setOnAction(event -> {
-                outputField.setText(outputField.getText() + btnText);
-            });
-            buttonGrid.add(numberButton, i, 2);
-        }
-
-        startInt = 1;
-        for (int i = 0; i < 3; i++) {
-            String btnText = String.valueOf(i + startInt);
-            // System.out.println("button text = " + btnText);
-            Button numberButton = new Button(btnText);
-            numberButton.setOnAction(event -> {
-                outputField.setText(outputField.getText() + btnText);
-            });
-            buttonGrid.add(numberButton, i, 3);
-        }
-
+        Button clearBtn = new Button("C");
+        Button buttonOne = new Button("1");
         Button zeroBtn = new Button("0");
-        zeroBtn.setOnAction(event -> {
-            outputField.setText(outputField.getText() + "0");
-        });
-        zeroBtn.setMaxWidth(Double.MAX_VALUE);
-        buttonGrid.add(zeroBtn, 0, 4, 2, 1);
-
         Button decimalBtn = new Button(" .");
-        decimalBtn.setOnAction(event -> {
-            outputField.setText(outputField.getText() + ".");
+        zeroBtn.setMaxWidth(Double.MAX_VALUE);
+
+        NumberFormat numberFormat = NumberFormat.getInstance(Locale.US);
+
+        buttonOne.setOnAction(event -> {
+
+            if (outputField.getText().isEmpty() || operationsField.getText() == null) { // outputField is empty
+                outputField.setText("1");
+            } else if (!outputField.getText().contains(".")) { // outputField contains no decimal
+
+                long safeNumber =  Long.parseLong(outputField.getText());
+                outputField.setText(numberFormat.format(safeNumber) + "1");
+            } else if (outputField.getText().endsWith(".")) { // outputField contains only a decimal point
+                String safeNumber = outputField.getText().replace(",", "");
+                long safeNumberLong = Long.parseLong(safeNumber);
+                outputField.setText(numberFormat.format(safeNumberLong) + ".1");
+            } else { // outputField has a decimal and trailing numbers
+                String safeNumber = outputField.getText().replace(",", "");
+                BigDecimal safeNumberBigDecimal = new BigDecimal(safeNumber);
+                outputField.setText(numberFormat.format(safeNumberBigDecimal) + "1");
+            }
         });
+
+        decimalBtn.setOnAction(event -> {
+            if (!outputField.getText().contains(".")) {
+                long safeNumber =  Long.parseLong(outputField.getText());
+                outputField.setText(numberFormat.format(safeNumber) + ".");
+            }
+        });
+
+        clearBtn.setOnAction(event -> {
+            outputField.setText("");
+        });
+        buttonGrid.add(clearBtn, 0, 0);
+        buttonGrid.add(buttonOne, 0, 1);
+        buttonGrid.add(zeroBtn, 0, 4, 2, 1);
         buttonGrid.add(decimalBtn, 2, 4);
 
         root.getChildren().addAll(outputBox, buttonGrid);
@@ -102,9 +88,45 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
+}
 
-    //    public boolean operationComplete(String input) {
+/**        int startInt = 7;
+ for (int i = 0; i < 3; i++) {
+ String btnText = String.valueOf(i + startInt);
+ // System.out.println("button text = " + btnText);
+ Button numberButton = new Button(btnText);
+ numberButton.setOnAction(event -> {
+ outputField.setText(outputField.getText() + btnText);
+ });
+ buttonGrid.add(numberButton, i, 1);
+ }
+
+ startInt = 4;
+ for (int i = 0; i < 3; i++) {
+ String btnText = String.valueOf(i + startInt);
+ // System.out.println("button text = " + btnText);
+ Button numberButton = new Button(btnText);
+ numberButton.setOnAction(event -> {
+ outputField.setText(outputField.getText() + btnText);
+ });
+ buttonGrid.add(numberButton, i, 2);
+ }
+
+ startInt = 1;
+ for (int i = 0; i < 3; i++) {
+ String btnText = String.valueOf(i + startInt);
+ // System.out.println("button text = " + btnText);
+ Button numberButton = new Button(btnText);
+ numberButton.setOnAction(event -> {
+ outputField.setText(outputField.getText() + btnText);
+ });
+ buttonGrid.add(numberButton, i, 3);
+ }
+ *
+ *
+ *
+  */
+//    public boolean operationComplete(String input) {
 //
 //        return input.matches(".*\\d$");
 //    }
-}
